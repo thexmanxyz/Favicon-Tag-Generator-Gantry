d13d0dad0df3b781f483dcc6d15f935a781e9963
thex <andreas.kar@gmx.at>
2018-05-27 22:45:18 +0200

    language / legacy support, prep v1.0.1

--- build/build.bat
+++ build/build.bat
@@ -14,21 +14,21 @@ REM #######################################################
 
 REM --- Script Variables ---
 set remove_folders=1
-set log_files=0
+set log_files=1
 set title_hr=-------------------------------------
 
 REM --- Packaging Variables ---
-set langs_upper=EN
+set langs_upper=EN, DE
 set default_lang=EN
 
-set prj_id=ccn
-set prj_rev=v1.0.0
+set prj_id=ftg
+set prj_rev=v1.0.1
 set prj_name=favicon-tag-generator
 set prj_fullname=Favicon Tag Generator - Extension
 
 set pkg_part_only=extension.only
 set pkg_def_enable=1
-set pkg_leg_enable=0
+set pkg_leg_enable=1
 set pkg_helium_enable=1
 set pkg_hydro_enable=1
 
@@ -38,6 +38,6 @@ set part_def_files=LICENSE, README.md
 set plugin_def_files=LICENSE.pdf
 REM set file_ext=yaml, html.twig
 set file_ext=
-set lang_ext=yaml
+set lang_ext=assets.yaml
 
 "base-build.bat"
